41413d073adb115211ca72b5a921cf837300c0bd
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-05 10:24:58 +0200

    Add application information

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,18 @@
 
 #include "mainwindow.h"
 
+using namespace Qt::Literals::StringLiterals;
+
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+    app.setOrganizationName("Ralf Stamm"_L1);
+    app.setOrganizationDomain("https://rstammdev.github.io"_L1);
+    app.setApplicationName("Sudoku-Qt"_L1);
+    app.setApplicationVersion("0.1.0"_L1);
+    app.setApplicationDisplayName("Sudoku-Qt"_L1);
+    app.setDesktopFileName("io.github.rstamm.sudoku_qt"_L1);
 
     MainWindow window;
     window.show();
