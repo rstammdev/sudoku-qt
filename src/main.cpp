2b4c7d3e695e41bebc5e24416218cc699883c7b1
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-14 11:00:13 +0200

    Fix code style

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 /**
  * SPDX-FileComment: Project Homepage: https://github.com/rstammdev/sudoku-qt
+ *
  * SPDX-FileCopyrightText: 2025 Ralf Stamm
+ *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,14 @@
 /**
  * SPDX-FileComment: Project Homepage: https://github.com/rstammdev/sudoku-qt
+ *
  * SPDX-FileCopyrightText: 2025 Ralf Stamm
+ *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #include "mainwindow.h"
 
+#include <QMenu>
 #include <QMenuBar>
 #include <QSettings>
 #include <QStatusBar>
