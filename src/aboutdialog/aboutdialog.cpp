903e9c153d5970d7bbed668abbf90976a0424ef8
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-15 16:37:06 +0200

    Add button 'Close'

--- src/aboutdialog/aboutdialog.cpp
+++ src/aboutdialog/aboutdialog.cpp
@@ -9,11 +9,21 @@
 #include "aboutdialog.h"
 
 #include <QApplication>
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
 
 
 AboutDialog::AboutDialog(QWidget* parent)
     : QDialog{parent}
 {
+
+    QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Close);
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addWidget(buttonBox);
+    setLayout(layout);
+
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &AboutDialog::close);
 
     setWindowTitle(tr("About %1").arg(QApplication::applicationName()));
     setMinimumSize(640, 480);
