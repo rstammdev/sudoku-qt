d57c01971572284a4cc91be666dd7124afa02c4e
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-20 17:00:26 +0200

    Add page header

--- src/settingsdialog/settingspageapplication.cpp
+++ src/settingsdialog/settingspageapplication.cpp
@@ -8,10 +8,29 @@
 
 #include "settingspageapplication.h"
 
+#include <QLabel>
+#include <QVBoxLayout>
+
 
 SettingsPageApplication::SettingsPageApplication(QWidget* parent)
     : SettingsPage{parent}
 {
+    QLabel* title = new QLabel(tr("<strong style=\"font-size: large;\">%1</strong>").arg(tr("Application")));
+
+    QLabel* description = new QLabel(tr("Configure the general settings of the application."));
+
+
+
+    QWidget* widget = new QWidget(parent);
+
+    QVBoxLayout* layout = new QVBoxLayout(widget);
+    layout->addWidget(title);
+    layout->addWidget(description);
+    layout->addStretch();
+
+    setWidget(widget);
+    setWidgetResizable(true);
+    setFrameShape(QFrame::NoFrame);
 
     setPageType(SettingsPage::PageTypeRoot);
     setPageTitle(tr("Application"));
