2bfd774084d829442857f69a902abdc220a09128
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-20 20:17:47 +0200

    Add button 'Close'

--- src/settingsdialog/settingsdialog.cpp
+++ src/settingsdialog/settingsdialog.cpp
@@ -9,11 +9,27 @@
 #include "settingsdialog.h"
 
 #include <QApplication>
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
 
 
 SettingsDialog::SettingsDialog(QWidget *parent)
     : QDialog{parent}
 {
+
+    // Buttons
+
+    QDialogButtonBox* buttonBox = new QDialogButtonBox;
+
+    buttonBox->addButton(QDialogButtonBox::Close);
+
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &SettingsDialog::close);
+
+    //
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addWidget(buttonBox);
+    setLayout(layout);
 
     setWindowTitle(tr("Configure %1").arg(QApplication::applicationName()));
     setMinimumSize(1024, 576);
