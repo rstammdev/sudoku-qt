b6bc33893b1ec6078c368410b7cb013f3a29a703
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-23 15:33:50 +0200

    Add page header

--- src/settingsdialog/settingspagecomponents.cpp
+++ src/settingsdialog/settingspagecomponents.cpp
@@ -8,10 +8,29 @@
 
 #include "settingspagecomponents.h"
 
+#include <QLabel>
+#include <QVBoxLayout>
+
 
 SettingsPageComponents::SettingsPageComponents(QWidget* parent)
     : SettingsPage{parent}
 {
+    QLabel* title = new QLabel(tr("<strong style=\"font-size: large;\">%1</strong>").arg(tr("Components")));
+    QLabel* description = new QLabel(tr("Specify which information in the components dialog is shown."));
+
+
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addWidget(title);
+    layout->addWidget(description);
+    layout->addStretch();
+
+    QWidget* widget = new QWidget(parent);
+    widget->setLayout(layout);
+
+    setWidget(widget);
+    setWidgetResizable(true);
+    setFrameShape(QFrame::NoFrame);
 
     setPageType(SettingsPage::PageTypeRoot);
     setPageTitle(tr("Components"));
